2eff1d18c383cc4c437d178cb6ed64a01482dffe
joaoadsistemas <joaoadsistemas@gmail.com>
2026-06-30 20:41:31 -0300

    feat(structure): initial structure project

---
+++ src/main/java/com/joao/hexagonal_credit_approval_service/HexagonalCreditApprovalServiceApplication.java
@@ -0,0 +1,13 @@
+package com.joao.hexagonal_credit_approval_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HexagonalCreditApprovalServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HexagonalCreditApprovalServiceApplication.class, args);
+	}
+
+}
